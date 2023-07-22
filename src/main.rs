f4eb97d9095c6de2df786bb9c4429d398b87bd6c
zy-Liang <zyliang@umich.edu>
2023-07-22 17:02:21 -0400

    update

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,12 @@
 pub mod syntax;
 
+mod test {
+    mod test_parse;
+}
+
 use lalrpop_util::lalrpop_mod;
 lalrpop_mod!(pub parser, "/parser.rs");
 
 fn main() {
-    let to_parse = vec!["1.90", "-1.0", "1+1", "2*5"];
-    for parse in to_parse {
-        if let Ok(res) = parser::PlusMinusParser::new().parse(parse) {
-            println!("{:?}", res);
-        } else {
-            println!("parse failed: {}", parse)
-        }
-    }
 }
+
---
+++ src/test/test_parse.rs
@@ -0,0 +1,101 @@
+use crate::parser;
+
+#[test]
+
+fn t1() {
+    let parse = "1";
+    let res = parser::ExprParser::new().parse(parse).unwrap();
+    println!("{}, {:?}", parse, res);
+}
+
+#[test]
+fn t2() {
+    let parse = "1.01";
+    let res = parser::ExprParser::new().parse(parse).unwrap();
+    println!("{}, {:?}", parse, res);
+}
+
+#[test]
+fn t3() {
+    let parse = "-1.01";
+    let res = parser::ExprParser::new().parse(parse).unwrap();
+    println!("{}, {:?}", parse, res);
+}
+
+#[test]
+fn t4() {
+    let parse = "1+1";
+    let res = parser::ExprParser::new().parse(parse).unwrap();
+    println!("{}, {:?}", parse, res);
+}
+
+#[test]
+fn t5() {
+    let parse = "1+1+1";
+    let res = parser::ExprParser::new().parse(parse).unwrap();
+    println!("{}, {:?}", parse, res);
+}
+
+#[test]
+fn t6() {
+    let parse = "1-1";
+    let res = parser::ExprParser::new().parse(parse).unwrap();
+    println!("{}, {:?}", parse, res);
+}
+
+#[test]
+fn t7() {
+    let parse = "1-1-1";
+    let res = parser::ExprParser::new().parse(parse).unwrap();
+    println!("{}, {:?}", parse, res);
+}
+
+#[test]
+fn t8() {
+    let parse = "1+1-1";
+    let res = parser::ExprParser::new().parse(parse).unwrap();
+    println!("{}, {:?}", parse, res);
+}
+
+#[test]
+fn t9() {
+    let parse = "1*1";
+    let res = parser::ExprParser::new().parse(parse).unwrap();
+    println!("{}, {:?}", parse, res);
+}
+
+#[test]
+fn t10() {
+    let parse = "1*1*1";
+    let res = parser::ExprParser::new().parse(parse).unwrap();
+    println!("{}, {:?}", parse, res);
+}
+
+#[test]
+fn t11() {
+    let parse = "1*1+1";
+    let res = parser::ExprParser::new().parse(parse).unwrap();
+    println!("{}, {:?}", parse, res);
+}
+
+#[test]
+fn t12() {
+    let parse = "1+1*1";
+    let res = parser::ExprParser::new().parse(parse).unwrap();
+    println!("{}, {:?}", parse, res);
+}
+
+#[test]
+fn t13() {
+    let parse = "---1";
+    let res = parser::ExprParser::new().parse(parse).unwrap();
+    println!("{}, {:?}", parse, res);
+}
+
+#[test]
+fn t14() {
+    let parse = "1+1+1-1*1*-1";
+    let res = parser::ExprParser::new().parse(parse).unwrap();
+    println!("{}, {:?}", parse, res);
+}
+
